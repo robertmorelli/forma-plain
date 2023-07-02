803d55fe2dd39fb144fee07ae4c3cdafed846ca1
Robert Morelli <robertondino@outlook.com>
2023-07-02 04:40:04 -0600

    move fin to docs

--- build_scripts/watcher.js
+++ build_scripts/watcher.js
@@ -12,7 +12,7 @@ try {
         'src/index.html',
     ]
     ).on("all", (e,p) => {
-        exec('npx html-inline-external --src src/index.html --dest fin/index.html --pretty', (err, _, __) => {
+        exec('npx html-inline-external --src src/index.html --dest docs/index.html --pretty', (err, _, __) => {
             if (err) return void console.log(err);
             //print the time of the update in hh:mm:ss format
             const now = new Date();
